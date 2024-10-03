4b25927ae9b8d9fd1e82616c7b3749698f7a69e1
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-10-03 14:03:35 +0700

    Update tpr_fpr.py

--- tpr_fpr.py
+++ tpr_fpr.py
@@ -30,7 +30,7 @@ def tpr_experiment():
     num_trials = 100
     num_reps = 10
     p = 5
-    lamda = 0.2
+    lamda = 0.05
     threshold = 20
     alpha = 0.05
     tpr_values_list = []
@@ -90,7 +90,7 @@ def fpr_experiment():
     n_list = [100, 200, 300, 400, 500]
     num_trials = 100
     p = 5
-    lamda = 0.2
+    lamda = 0.05
     threshold = 20
     alpha = 0.05
     fpr_values_list = []
