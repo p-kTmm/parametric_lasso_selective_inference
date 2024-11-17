6d2dfbd5a541fa93c11d70754dc30c95a90d1df7
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-11-17 21:18:35 +0700

    Update uniform.py

--- uniform.py
+++ uniform.py
@@ -8,9 +8,9 @@ import util
 
 def run_simulation(num_simulations=1000):
     n = 100
-    p = 5
+    p = 10
     lamda = 0.05
-    beta_vec = [0, 0, 0, 0, 0]  # Under the null hypothesis
+    beta_vec = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # Under the null hypothesis
 
     cov = np.identity(n)
     threshold = 20
