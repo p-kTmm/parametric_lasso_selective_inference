6cdb589d01ef92494a2f520b088d4f3b503cdede
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-11-17 22:03:54 +0700

    Update uniform_trans.py

--- uniform_trans.py
+++ uniform_trans.py
@@ -62,9 +62,8 @@ def run_simulation(num_simulations=1000):
     s=0
     n0 = 150
     M = 8
-    lamda = 0.08
-    lamda_w = lamda
-    lamda_delta = lamda
+    lamda = lamda_w = lamda_delta = 0.08
+
     # lamda_w=0.1
     # lamda_delta=0.05
   
@@ -74,9 +73,9 @@ def run_simulation(num_simulations=1000):
     p_values = []
     cov = np.identity(n0)    
     for sim in range(num_simulations):
-        X, y, n_vec, _ = gen_data.generate_data(p=p, n0=n0, M=M, s=s, sig_beta=sig_beta, gamma=gamma, lamda_w=lamda_w, lamda_delta=lamda_delta)
+        X, y, n_vec, _ = gen_data.generate_data(p=p, n0=n0, M=M, s=s, sig_beta=sig_beta, gamma=gamma)
 
-        bh = gen_data.OracleTransLasso(X, y, n_vec)
+        bh = gen_data.OracleTransLasso(X, y, n_vec, lamda_w=lamda_w, lamda_delta=lamda_delta)
         X, y = X[:n_vec[0]], y[:n_vec[0]]
         y = y.reshape((-1, 1))
         A, XA, Ac, XAc, bhA = util.construct_A_XA_Ac_XAc_bhA(X, bh, n0, p)
