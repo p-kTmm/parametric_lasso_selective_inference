3808a051fc59beaa44ac2a0eb9baa21dc9668e8d
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-11-17 20:55:56 +0700

    Update uniform_trans.py

--- uniform_trans.py
+++ uniform_trans.py
@@ -1,77 +1,39 @@
 import numpy as np
-from sklearn import linear_model
+from sklearn.linear_model import Lasso
 import matplotlib.pyplot as plt
 
 import parametric_lasso
-import gen_data
 import util
-import warnings
-from sklearn.exceptions import ConvergenceWarning
-
-# Tắt cảnh báo ConvergenceWarning
-warnings.filterwarnings("ignore", category=ConvergenceWarning)
+import gen_data
 
 def run_simulation(num_simulations=1000):
-    # Parameters
-    alpha = 0.1
-    p = 200
-    # s = 16
-    s = 0
-    M = 20
-    sig_beta = 0.3
+    p = 20
+    s=0
     n0 = 150
-    n_vec = [n0] + [100] * M
-    size_A0 = 12
-    h = 6
-    q = 2 * s
-    sig_delta1 = sig_beta
-    sig_delta2 = sig_beta + 0.2
-    exact = False
-    
-    # Define A0 (list of relevant task indices)
-    A0 = list(range(size_A0))
-
-    # n = n_vec[0]
-    n = sum(n_vec)
-    cov = np.identity(n)
+    M = 6
+    lamda = 0.05
+    sig_beta = 0.3
+    gamma = 0.3
     threshold = 20
     p_values = []
-    lamda = alpha
-
-    
+    cov = np.identity(n0)
     for sim in range(num_simulations):
-        X, y, y_true, beta0 = gen_data.gen_data_transfer(n_vec=n_vec, s=s, h=h, q=q, size_A0=size_A0, M=M, 
-                                 sig_beta=sig_beta, sig_delta1=sig_delta1, 
-                                 sig_delta2=sig_delta2, p=p, exact=exact)
-        # X, y = X[:n_vec[0]], y[:n_vec[0]]
-        # clf = linear_model.Lasso(alpha=lamda, fit_intercept=False, tol=1e-10)
-        # clf.fit(X, y)
-        # bh = clf.coef_
-        # print(bh)
-
-
-        res_kA = gen_data.las_kA(X, y, A0=A0, n_vec=n_vec, alpha=alpha)
-        bh = res_kA['beta_kA']
-        
-        y = y.reshape((n, 1))
-
-        A, XA, Ac, XAc, bhA = util.construct_A_XA_Ac_XAc_bhA(X, bh, n, p)
+        X, y, n_vec, _ = gen_data.generate_data(p=p, n0=n0, M=M, s=s, sig_beta=sig_beta, gamma=gamma)
+        bh = Lasso(alpha=lamda, fit_intercept=False).fit(X[:n_vec[0]], y[:n_vec[0]]).coef_
+        y = y.reshape((-1, 1))
+        A, XA, Ac, XAc, bhA = util.construct_A_XA_Ac_XAc_bhA(X, bh, n0, p)
 
         if len(A) == 0:
             continue
 
         for j_selected in A:
             etaj, etajTy = util.construct_test_statistic(j_selected, XA, y, A)
-
             list_zk, list_bhz, list_active_set = parametric_lasso.run_parametric_lasso(
-                X, y, lamda, etaj, n, p, threshold)
+                X, y, lamda, etaj, n0, p, threshold)
             p_value = util.p_value(
                 A, bh, list_active_set, list_zk, list_bhz, etaj, etajTy, cov)
+            p_values.append(p_value)
 
-            if p_value:
-                p_values.append(p_value)
-
-    # Plot the histogram of p-values
     plt.hist(p_values, bins=20, edgecolor='k', density=True)
     plt.xlabel('p-value')
     plt.ylabel('Density')
